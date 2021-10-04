339120b107f3dd78f6c6ba9878e099ad9f2b86f7
JRaymond87 <garishua@gmail.com>
2021-10-03 20:30:51 -0500

    Agragando metodos a GeContacto y al diagamasclases

--- Smart Scheduler/src/uam/azc/adsi/smartscheduler/operations/GestorContacto.java
+++ Smart Scheduler/src/uam/azc/adsi/smartscheduler/operations/GestorContacto.java
@@ -1,14 +1,16 @@
 package uam.azc.adsi.smartscheduler.operations;
+
 import java.util.LinkedList;
+import uam.azc.adsi.smartscheduler.classes.Contacto;
 
 public class GestorContacto {
-    ArrayList<Contacto> listaContactos = new ArrayList();
+    LinkedList<Contacto> listaContactos = new LinkedList();
 
     // metodo de cargar el archivo que regrese la lista de contactos
     //listaContactos = funcionCargaArchivo()
 
     // funcion de agregar contacto que recibe un objeto de tipo contacto y lo agrega al arreglo
-    public boolean agregarContacto(Contacto c){
+    public boolean cContact(Contacto c){
         try{
             listaContactos.add(c);
             // hacer sort a arreglo para que al cargar la lista de nuevo el contacto agregado no
@@ -19,8 +21,20 @@ public class GestorContacto {
     }
 
     // funcion de editar un contacto 
-    public boolean editarContacto(Contacto c){
+    public boolean eContact(Contacto c){
         // buscar el contacto en la lista
-        
+        return true;
+    }
+
+    public boolean dContact(Contacto c){
+        return true;
+    }
+
+    public boolean searchDup(LinkedList<Contacto> t){
+    return true;
+    }
+
+    public boolean searchInc(LinkedList<Contacto> t){
+        return true;
     }
 }
