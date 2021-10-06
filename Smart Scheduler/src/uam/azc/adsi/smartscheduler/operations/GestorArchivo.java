b55d4f5a3b02451a6785f90e00e2b0c49bcdecb5
JuanCarlosGarciaZepeda <2183033750@cua.uam.mx>
2021-10-06 10:56:16 -0500

    comentados metodos de parser

--- Smart Scheduler/src/uam/azc/adsi/smartscheduler/operations/GestorArchivo.java
+++ Smart Scheduler/src/uam/azc/adsi/smartscheduler/operations/GestorArchivo.java
@@ -7,9 +7,12 @@ import java.io.IOException;
 import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 
+import uam.azc.adsi.smartscheduler.classes.Contacto;
+
+
 public class GestorArchivo {
 
-    /* Recupera los clientes del archivo */
+    /* Recupera los contactos del archivo vcf*/
     public static void leerArchivo() {
 
         String cadenaLeida = "";
@@ -19,26 +22,37 @@ public class GestorArchivo {
             BufferedReader archivoLectura = new BufferedReader(fr);
             cadenaLeida = archivoLectura.readLine();
 
+            // mientras no se encuentre el final del archivo
             while (cadenaLeida != null) {              
 
+                // se se encuentra una etiqueta de inicio 
                 if (cadenaLeida.equals("BEGIN:VCARD")) {
-                    System.out.println(archivoLectura.readLine());
+                    System.out.println(archivoLectura.readLine());  // lee la linea de version
 
+                    Contacto c = new Contacto();
+
+                    // mientras no se encuentre una etiqueta de cierre
                     while (!cadenaLeida.equals("END:VCARD")) {
-                        StringTokenizer st = new StringTokenizer(cadenaLeida, ":");
-                        StringTokenizer st3 = new StringTokenizer(cadenaLeida, ";");
-                        String etiqueta = st.nextToken();
-                        String etiqueta2 = st3.nextToken();
+                        StringTokenizer st = new StringTokenizer(cadenaLeida, ":"); // tokenizer para :
+                        StringTokenizer st3 = new StringTokenizer(cadenaLeida, ";");// tokenizer para ;
 
+                        // se lee el tipo de etiqueta que este en la actual linea leida del archivo
+                        String etiqueta = st.nextToken();   // hasta el primer :
+                        String etiqueta2 = st3.nextToken(); // hasta el primer ;
+
+                        // si se encuentra una etiqueta de N
                         if (etiqueta.equals("N")) {
                             System.out.println("\nEtiqueta N");
 
+                            // se elimina del renglon leido la etiqueta y el token para poder leer el siguiente contenido
                             cadenaLeida = cadenaLeida.replaceAll(etiqueta + ":", " ");
                             StringTokenizer st2 = new StringTokenizer(cadenaLeida, ";");
 
+                            // se obtienen los contenidos de la etiqueta mediante el token ;
                             String apellido = st2.nextToken();
                             if (apellido.equals(" ")) {
                                 System.out.println("Apellido esta vacio");
+                                
                             } else {
                                 System.out.println("Apellido: " + apellido.trim());
                             }
@@ -66,6 +80,7 @@ public class GestorArchivo {
                                 System.out.println("Grado esta vacio");
                             }
 
+                        // si la etiqueta es FN
                         } else if (etiqueta.equals("FN")) {
                             System.out.println("\nEtiqueta FN");
 
@@ -79,6 +94,21 @@ public class GestorArchivo {
                                 System.out.println("nombreCompleto esta vacio");
                             }
 
+                        // si la etiqueta es ORG
+                        } else if (etiqueta.equals("ORG")) {
+                            System.out.println("\nEtiqueta ORG");
+
+                            cadenaLeida = cadenaLeida.replaceAll(etiqueta + ":", "");
+                            StringTokenizer st2 = new StringTokenizer(cadenaLeida, ";");
+
+                            try {
+                                String organizacion = st2.nextToken();
+                                System.out.println("organizacion: " + organizacion);
+                            } catch (Exception e) {
+                                System.out.println("organizacion esta vacio");
+                            }
+
+                        // si la etiqueta es TEL
                         } else if (etiqueta2.equals("TEL")) {
                             System.out.println("\nEtiqueta TEL");
 
@@ -94,6 +124,7 @@ public class GestorArchivo {
                                 System.out.println("telefono esta vacio");
                             }
 
+                        // si la etiqueta es EMAIL
                         } else if (etiqueta2.equals("EMAIL")) {
                             System.out.println("\nEtiqueta EMAIL");
 
@@ -107,6 +138,8 @@ public class GestorArchivo {
                             } catch (Exception e) {
                                 System.out.println("Email esta vacio");
                             }
+
+                        // si la etiqueta es ADR
                         } else if (etiqueta2.equals("ADR")) {
                             System.out.println("\nEtiqueta ADR");
 
@@ -164,16 +197,21 @@ public class GestorArchivo {
                             } catch (Exception e) {
                                 System.out.println("pais vacio");
                             }
-                                          
+                            
+                        // si la etiqueta es PHOTO
                         } else if (etiqueta2.equals("PHOTO")) {
                             System.out.println("\nEtiqueta PHOTO");
+
+                            cadenaLeida = cadenaLeida.replaceAll(etiqueta2 + ";", "");
 
                             String encoding = st3.nextToken();
                             cadenaLeida = cadenaLeida.replaceAll(encoding + ";", "");
                             
                             StringTokenizer st2 = new StringTokenizer(cadenaLeida, ":");
-                            String tipo = st2.nextToken();
+                            cadenaLeida = cadenaLeida.replaceAll(encoding + ":", "");
 
+                            String tipo = st2.nextToken();
+                            
                             cadenaLeida = cadenaLeida.replaceAll(tipo + ":", "");
 
                             String base64 = "";
