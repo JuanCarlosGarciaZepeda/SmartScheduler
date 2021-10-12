44324944f7bf25fadc360d853008b12777e62ba1
Lunaticals <86329621+Lunaticals@users.noreply.github.com>
2021-10-12 11:15:35 -0500

    Added nTableView mouseClicked event handler.

--- Smart Scheduler/src/uam/azc/adsi/smartscheduler/gui/FXMLPrincipalController.java
+++ Smart Scheduler/src/uam/azc/adsi/smartscheduler/gui/FXMLPrincipalController.java
@@ -73,7 +73,14 @@ public class FXMLPrincipalController implements Initializable {
         
         nTableView.setVisible(true);
         
+        nTableView.setOnMouseClicked(e -> {evento();});
+        
     }
+    
+    private void evento(){
+        menuComboBox.getSelectionModel().select("Telefonos");
+        muestraTablaTel();
+    };
     
     @FXML
     public void muestraTablaDir(){
